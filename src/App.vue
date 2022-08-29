9612a54497dd9bc723b2e2ece3d78eaaf7e0dddd
Jordan Ubilla <jordan.ubilla@funiber.org>
2022-08-29 14:08:06 -0500

    init code

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar dense elevation="0">
+        <v-spacer></v-spacer>
+        <v-avatar color="red" height="40" min-width="40" width="40">
+          ju
+        </v-avatar>
+      </v-app-bar>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
